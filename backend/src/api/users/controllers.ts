5c6770dc5f26b6e1d47608b3ce8b81dd704d7348
agnoam <agnoam.aghai.ooo.123@gmail.com>
2024-10-31 16:16:20 +0200

    Added login function that checks the `authorization` header in the request

--- backend/src/api/users/controllers.ts
+++ backend/src/api/users/controllers.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from 'express';
+import { DecodedIdToken } from 'firebase-admin/auth';
+
+import { firebaseAuth } from '../../config/firebase';
+import { ResponseStatus } from '../../constants/constants';
 
 /* 
     This file contains the routes validation implementation
     The convention for route handler is _R at the end of the function name
 */
+export const loginUser_R = async (req: Request, res: Response) => {
+    console.log('executing loginUser_R()');
+
+    const authorizationCode: string = req.get('authorization');
+    if (!authorizationCode) {
+        res.status(ResponseStatus.BadRequest).json({ description: 'Authorization code must be provided' });
+        return;
+    }
+
+    try {
+        const decodedToken: DecodedIdToken = await firebaseAuth.verifyIdToken(authorizationCode);
+
+        // TODO: Create user in database
+
+        res.status(ResponseStatus.Ok).json({ description: 'hello', uid: decodedToken.uid, exp: decodedToken.exp, picture: decodedToken.picture });
+    } catch (ex) {
+        console.error(ex);
+        res.status(ResponseStatus.InternalError).json({ description: 'Error occured while login' });
+    }
+}
 
 export const getUser_R = (req: Request, res: Response) => {
     res.send(`Fetching ${req.params.userID}`);
--- backend/src/api/users/routes.ts
+++ backend/src/api/users/routes.ts
@@ -2,10 +2,12 @@
 
 import { Router } from "express";
 
-import { getUser_R, createUser_R, deleteUser_R, updateUser_R } from "./controllers";
+import { loginUser_R, getUser_R, createUser_R, deleteUser_R, updateUser_R } from "./controllers";
 
+console.log('Initializing users router');
 const router: Router = Router();    
 
+router.post('/login', loginUser_R);
 router.get('/:userID', getUser_R);
 router.post('/', createUser_R);
 router.put('/:userID', updateUser_R);
